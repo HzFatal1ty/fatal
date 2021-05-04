be886975fefa369f975a5d8c3b1dbadf84f9b605
HzFatal1ty <83640114+HzFatal1ty@users.noreply.github.com>
2021-05-04 12:20:35 +0300

    Fatal.sh
    :)

---
+++ Fatal.sh
@@ -0,0 +1,12 @@
+termux-toast -b red -c yellow "Merhaba"
+	sleep 2
+	termux-toast -b red -c yellow  "Ben Devrim Saitoğlu 'nun yapay zekasıyım."
+	sleep 2
+	termux-toast -b red -c yellow  "Buradan @yusfinos @hech.px selamım olsun. Şimdi bir tool kuracağım."
+	sleep 4
+	termux-toast -b red -c yellow  "Tool bittiğinde sana bir bildirim göndermiş olucağım"
+	sleep 3
+	termux-toast -b red -c yellow  "Hadi başlıyalım bakalım :)"
+	sleep 2
+	termux-toast -b red -c yellow  "F4TAL SUNAR.."
+	sleep 2
